ead0674384b7e51f045b1cdb81e54942281d4108
Paige Kohn <paigkay@gmail.com>
2018-12-26 11:28:13 -0500

    added gif

--- EncryptionApp/app/src/main/java/com/example/paige/encryptionapp/MainActivity.java
+++ EncryptionApp/app/src/main/java/com/example/paige/encryptionapp/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.paige.encryptionapp;
 
 
 import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
-
+import static com.bumptech.glide.request.RequestOptions.centerCropTransform;
 import android.os.Environment;
 import android.os.StrictMode;
 import android.support.annotation.NonNull;
@@ -13,13 +16,21 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static android.net.http.SslCertificate.restoreState;
@@ -29,8 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE = 34783;
 
-    private List<File> mDirectories = new ArrayList<>();
-
+    private ImageView mImageView;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -42,9 +52,11 @@ public class MainActivity extends AppCompatActivity {
                     switchFragment(new EncryptFragment());
                     return true;
                 case R.id.navigation_decrypt:
+                    changeDimensions();
                     switchFragment(new DecryptFragment());
                     return true;
                 case R.id.navigation_directory:
+                    changeDimensions();
                     switchFragment(new DirectoryFragment());
 
             }
@@ -59,20 +71,38 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
         getWindow().setBackgroundDrawableResource(R.color.colorBackground);
-        if (savedInstanceState != null) {
-            restoreState(savedInstanceState);
-        } else {
-            mDirectories.add(Environment.getExternalStorageDirectory());
-        }
-
 
         String[] permissions = {"android.permission.WRITE_EXTERNAL_STORAGE"};
         requestPermissions(permissions, REQUEST_CODE);
 
         BottomNavigationView navigation =  findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        loadImage();
+    }
 
 
+   public void loadImage(){
+
+        mImageView = (ImageView) findViewById(R.id.backgroundGif);
+       String imageUrl = getURLForResource(R.drawable.background_main);
+
+       Glide.with(this)
+               .load(imageUrl)
+               .into(mImageView);
+
+
+
+
+   }
+
+    public String getURLForResource (int resourceId) {
+        return Uri.parse("android.resource://"+R.class.getPackage().getName()+"/" +resourceId).toString();
+    }
+
+
+    public void changeDimensions(){
+        mImageView.getLayoutParams().height = 0;
+        mImageView.getLayoutParams().width = 0;
     }
 
 
